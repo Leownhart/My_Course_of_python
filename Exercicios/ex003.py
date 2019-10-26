dab20c0a7be9bae4ffb5ffd67aeb7a07a8badbf4
Francisco <francisco.amartins.al@gmail.com>
2019-10-26 16:17:31 -0300

    Estudos até  a aula 06

---
+++ Exercicios/ex003.py
@@ -0,0 +1,6 @@
+n1 = int(input('Informe um Valor: '))
+n2 = int(input('Informe outro Valor: '))
+soma = n1 + n2
+print('A soma dos valores informados é : {}'.format(n1+n2))
+print('A soma entre {} e {} é igual a {}!'.format(n1, n2, soma))
+
