8481d265eb658ed71d4099c48d394b7c9f8b1345
Francisco <francisco.amartins.al@gmail.com>
2019-10-27 13:09:32 -0300

    Média de duas notas

---
+++ Exercicios/ex007.py
@@ -0,0 +1,5 @@
+NOne = float(input('Informe a primeira nota: '))
+NTwo = float(input('Informe a segunda nota: '))
+Med = (NOne + NTwo) / 2
+print('A média entre {:.1f} e {:.1f} é igual a {:.1f}'.format(NOne, NTwo, Med))
+
