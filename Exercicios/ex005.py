de060f1a6fbd24af2d622a91ad2be450540b7333
Francisco <francisco.amartins.al@gmail.com>
2019-10-27 12:12:07 -0300

    Sucessor é Antecessor

---
+++ Exercicios/ex005.py
@@ -0,0 +1,8 @@
+Number = int(input('Digite um número: '))
+Ant = Number - 1
+Suc = Number + 1
+print('Analisando o valor {}, seu antecessor é '
+      '{} e o sucessor é {} '.format(Number, Ant, Suc))
+
+
+
