f98f552be99fcf77103a96f6f6a8d1d8cdc80978
Francisco <francisco.amartins.al@gmail.com>
2019-10-31 18:20:24 -0300

    Preco

--- Exercicios/ex0013.py
+++ Exercicios/ex0013.py
@@ -1,6 +1,15 @@
-Sal = float(input('Qual é o salário do Funcionário?  R$ '))
-Percent = 15.0
-Acrescimo = Sal * Percent / 100
-NewSal = Sal + Acrescimo
-print('Um funcionário que ganhava R${:.2f}, com {:.0f}% de aumento, '
-      'passa a receber R${:.2f}'.format(Sal, Percent, NewSal))
+#Sal = float(input('Qual é o salário do Funcionário?  R$'))
+#Acrescimo = Sal * 15.0 / 100
+#NewSal = Sal + Acrescimo
+#print('Um funcionário que ganhava R${:.2f}, com 15% de aumento, '
+# 'passa a receber R${:.2f}'.format(Sal, NewSal))
+
+Preco = float(input('Informe o o preço do produto: R$'))
+PrecoDesc =  Preco - (Preco * 10 / 100)
+PrecoAcres = Preco + (Preco * 8 / 100)
+print('{}R$ com desconto de 10% é igual a {:.2f}R$'.format(Preco, PrecoDesc))
+print('{}R$ com acrescimo de 8% é igual a {:.2f}R$'.format(Preco, PrecoAcres))
+
+
+
+
