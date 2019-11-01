8cae037c30b08d75b7e7f41ec2bfbf565d908b94
Francisco <francisco.amartins.al@gmail.com>
2019-11-01 15:30:52 -0300

    Emoji

---
+++ PythonTest/ModTest.py
@@ -0,0 +1,4 @@
+from math import sqrt, floor
+num = int(input('Informe um número: '))
+raiz = sqrt(num)
+print('A raiz quadrada de {} é igual a {:.2f}'.format(num, floor(raiz)))
---
+++ PythonTest/Radom.py
@@ -0,0 +1,6 @@
+import emoji
+print(emoji.emojize("Olá, Mundo :sunglasses:", use_aliases=True))
+
+#import random
+#num = random.randint(1, 10)
+#print(num)
