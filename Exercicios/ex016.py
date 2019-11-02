67de21f7f8d05d233ab1c632ccbd79e42e0de45b
Francisco <francisco.amartins.al@gmail.com>
2019-11-01 22:54:21 -0300

    Trunc

---
+++ Exercicios/ex016.py
@@ -0,0 +1,9 @@
+'''from math import trunc
+Num = float(input('Informe um valor: '))
+inteiro = trunc(Num)
+print('O valor informado foi {} '
+      'e a sua porção inteira é {}'.format(Num, inteiro))'''
+
+Num = float(input('Informe um valor: '))
+print('O valor digitado foi {} '
+      'e a sua porção inteira é {}'.format(Num, int(Num)))
