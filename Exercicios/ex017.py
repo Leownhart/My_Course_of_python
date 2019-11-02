439abda724d95d0178e3ae90b095836a6df34449
Francisco <francisco.amartins.al@gmail.com>
2019-11-01 23:36:38 -0300

    Mod Catetos

--- Exercicios/ex017.py
+++ Exercicios/ex017.py
@@ -1,5 +1,6 @@
 from math import pow, sqrt
 oposto = float(input('Comprimento do cateto oposto: '))
-ajacente = float(input('Comprimento do cateto adjacente: '))
-hipot = sqrt(pow(oposto, 2) + pow(ajacente, 2))
+adjacente = float(input('Comprimento do cateto adjacente: '))
+hipot = sqrt(pow(oposto, 2) + pow(adjacente, 2))
+# ou (oposto ** 2 + adjacente ** 2) ** (1/2)
 print('A hipotenusa vai medir {:.2f}'.format(hipot))
