4bdba2acd3937750c3aaa28911c739bf6afa087b
Francisco <francisco.amartins.al@gmail.com>
2019-11-02 22:30:41 -0300

    Músic

---
+++ Exercicios/ex021.py
@@ -0,0 +1,13 @@
+import playsound
+playsound.playsound('ex021.mp3')
+
+#from pygame import mixer
+#mixer.init()
+#mixer.music.load('ex021.mp3')
+#mixer.music.play()
+
+#import pygame
+#pygame.init()
+#pygame.mixer.music.load('ex021.mp3')
+#pygame.mixer.music.play()
+#pygame.event.wait()
