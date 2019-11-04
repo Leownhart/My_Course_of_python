33989cd5b663035395d3da5602d9708695b04a47
Francisco <francisco.amartins.al@gmail.com>
2019-11-03 22:09:07 -0300

    Manipulando Strings

--- MString.py
+++ MString.py
@@ -1,5 +1,24 @@
-frase = 'Curso em Video Python'
-print(frase[3:13])
-print(frase[13:])
-print(frase[0:15])
-print(frase[::2])
+frase = ' Curso em Video Python '
+#print(frase[3:13])
+#print(frase[13:])
+#print(frase[0:15])
+#print(frase[::2])
+#print('Oi')
+#print(frase.count('o'))
+#print(frase.upper().count('O'))
+#print(len(frase.strip()))
+#print(frase.replace('Curso', 'Aula'))
+print('Curso' in frase)
+print(frase.find('Curso'))
+print(frase.split()) # Lista
+dividido = frase.split()
+print(dividido)
+print(dividido[0])
+print(dividido[2] [3])
+print('''Welcome! Are you completely new to programming?
+If not then we presume you will be looking for information
+about why and how to get started with Python. Fortunately
+an experienced programmer in any programming language
+(whatever it may be) can pick up Python very quickly.
+Its also easy for beginners to use and learn, so jump in!''')
+
