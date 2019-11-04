6c04f53f23c097b20a60f3ea697f33cc4eba12b5
Francisco <francisco.amartins.al@gmail.com>
2019-11-04 14:13:57 -0300

    Em que cidade

---
+++ Exercicios/ex024.py
@@ -0,0 +1,7 @@
+cid = str(input('Em que cidade você nasceu? ')).strip()
+print(cid[:5].upper() == 'SANTO')
+
+# cida = str(input('Em qual cidade você nasceu: ')).strip() # RETIRA OS ESPAÇOS DO COMEÇO E DO FIM
+# listnome = cida.split() # CRIA UMA LISTA
+# print('Santo' in listnome[0]) # VERIFICA UMA DETERMINADA STRING NA LISTA
+
