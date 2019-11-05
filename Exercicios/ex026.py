6c926ca46c427ca50e257e8e774a758d009f22e7
Francisco <francisco.amartins.al@gmail.com>
2019-11-05 13:14:55 -0300

    Paula Galvão

---
+++ Exercicios/ex026.py
@@ -0,0 +1,7 @@
+frase = str(input('Digite uma frase: ')).strip()
+a = frase.lower().count('a')
+print('A letra A apareceu {} vezes na frase'.format(a))
+b = frase.upper().find('A') + 1
+print('A primeira letra A apareceu na posição {}'.format(b))
+c = frase.upper().rfind('A') + 1
+print('A última letra A apareceu na posição {}'.format(c))
