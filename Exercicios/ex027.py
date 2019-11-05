059c693cd09d883f8b5eae238834f166c9dcf85d
Francisco <francisco.amartins.al@gmail.com>
2019-11-05 14:00:45 -0300

    Primeiro é último

--- Exercicios/ex027.py
+++ Exercicios/ex027.py
@@ -3,6 +3,5 @@ print('Muito prazer em te conhecer! ')
 print('Seu primeiro nome é {}'.format(nome[0]))
 nome.reverse()
 print('Seu último nome é {}'.format(nome[0]))
-
-
+# Podemos usar o método "len" como alternativa
 
