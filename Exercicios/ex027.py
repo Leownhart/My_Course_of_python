992aeb5da43524642f049d17faaffc7430f39520
Francisco <francisco.amartins.al@gmail.com>
2019-11-05 13:51:35 -0300

    Primeiro é último

---
+++ Exercicios/ex027.py
@@ -0,0 +1,8 @@
+nome = str(input('Digite seu nome completo: ')).strip().split()
+print('Muito prazer em te conhecer! ')
+print('Seu primeiro nome é {}'.format(nome[0]))
+nome.reverse()
+print('Seu último nome é {}'.format(nome[0]))
+
+
+
