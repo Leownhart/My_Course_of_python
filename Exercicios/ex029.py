0b0f6cb4ac61f20ba11cf07d6b81ed38a49554d6
Francisco <francisco.amartins.al@gmail.com>
2019-11-06 01:07:54 -0300

    Multa

--- Exercicios/ex029.py
+++ Exercicios/ex029.py
@@ -3,8 +3,7 @@ km = float(input('Qual é a velocidade atual do carro? '))
 if km <= 80:
     print('Tenha um bom dia! Dirija com segurança')
 else:
-    multa = km - 80
-    multa = multa * 7.0
+    multa = (km - 80) * 7
     print('MULTADO! Você execedeu o limite permitido que é 80km/h')
     print('Você deve pagar uma multa de R${:.2f}!'.format(multa))
     print('Tenha um bom dia! Dirija com segurança!')
