0165779b6719b61c5227a34d1708a26e332222d1
Francisco <francisco.amartins.al@gmail.com>
2019-11-07 16:40:30 -0300

    Viagem

--- Exercicios/ex031.py
+++ Exercicios/ex031.py
@@ -1,11 +1,9 @@
 Via = int(input('Qual é a distância da sua viagem? '))
 print('Você está prestes a começar uma viagem de {:.1f}km.'.format(Via))
 if Via <= 200:
-    Via = Via * 0.50
-    print('O preço da sua passagem será {:.2f}'.format(Via))
+    preco = Via * 0.50
 else:
-    Via = Via * 0.45
-    print('O preco da sua passagem será {:.2f}'.format(Via))
-
-# Mneira Simplificada
-# 
+    preco = Via * 0.45
+print('O preco da sua passagem será {:.2f}'.format(preco))
+# Maneira Simplificada
+# preço = distancia * 0.50 if distancia <= 200 else distancia * 0.45
