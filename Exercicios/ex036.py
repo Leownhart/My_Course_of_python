ccbe81284c3167ad1e132ddb263e0a7c6f3a927a
Francisco <francisco.amartins.al@gmail.com>
2019-11-13 15:06:32 -0300

    Prestação de um imovel

--- Exercicios/ex036.py
+++ Exercicios/ex036.py
@@ -1,7 +1,7 @@
 valorcasa = float(input('Informe o valor da imovel: R$ '))
 salcom = float(input('Informe o sálario do comprador: R$ '))
 anos = int(input('Informe o tempo de financiamento em anos: '))
-valpresta = (valorcasa / anos) / 12
+valpresta = (valorcasa / anos) / 12 # casa / (anos / * 12)
 porcent = salcom * 30.0 / 100
 print('Para pagar uma casa de R${:.2f} em {} anos a '
       'prestação será de R${:.2f} mensais'.format(valorcasa, anos, valpresta))
