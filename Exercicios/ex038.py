7ad27c414d30032ee3a5e3c85752a54064f7797c
Francisco <francisco.amartins.al@gmail.com>
2019-11-14 00:34:30 -0300

    Maior e Menor

---
+++ Exercicios/ex038.py
@@ -0,0 +1,8 @@
+number1 = int(input('Primeiro número: '))
+number2 = int(input('Segundo: '))
+if number1 > number2:
+    print('O PRIMEIRO valor é maior')
+elif number2 > number1:
+    print('O SEGUNDO valor é maior')
+else:
+    print('Os dois valores são iguais')
