9ec3111d80f69bebe36f46a5aeb999b26cff949e
Francisco <francisco.amartins.al@gmail.com>
2019-11-14 01:15:08 -0300

    Alistamento

--- Exercicios/ex039.py
+++ Exercicios/ex039.py
@@ -5,8 +5,10 @@ saldo = anoatual - nasc
 if anoatual - nasc < 18:
     print('Canditado ainda não está na idade de alistamento')
     print('Faltou {} anos para idade ideal para alistamento'.format(18 - saldo))
+    print('Seu alistamento será em {}'.format(anoatual + 18 - saldo))
 elif anoatual - nasc == 18:
     print('Canditato está na idade ideal para alistamento')
 else:
     print('Candidato passou do tempo de alistamento')
     print('Passou {} anos da idade ideal para alistamento'.format(saldo - 18))
+    print('Seu alismento deveria ser em {}'.format(anoatual - (saldo - 18)))
