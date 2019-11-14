7eb140484a60e5cbf2a609aa7125cd501774cffb
Francisco <francisco.amartins.al@gmail.com>
2019-11-14 01:01:27 -0300

    Alistamento

---
+++ Exercicios/ex039.py
@@ -0,0 +1,12 @@
+from datetime import date
+nasc = int(input('Informe o ano de nascimento: '))
+anoatual = date.today().year
+saldo = anoatual - nasc
+if anoatual - nasc < 18:
+    print('Canditado ainda não está na idade de alistamento')
+    print('Faltou {} anos para idade ideal para alistamento'.format(18 - saldo))
+elif anoatual - nasc == 18:
+    print('Canditato está na idade ideal para alistamento')
+else:
+    print('Candidato passou do tempo de alistamento')
+    print('Passou {} anos da idade ideal para alistamento'.format(saldo - 18))
