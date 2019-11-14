d7895606831541b06301e94322d812760bef38fa
Francisco <francisco.amartins.al@gmail.com>
2019-11-14 16:04:41 -0300

    Triângulos

--- Exercicios/ex042.py
+++ Exercicios/ex042.py
@@ -11,15 +11,16 @@ r3 = float(input('Terceiro Segmento: '))
 
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
     print('\033[33mOs segmentos acima podem formar um triângulo!\033[m')
-else:
-    print('\033[31mOs segmentos acima não podem formar um triângulo\033[m')
-
 if r1 == r2 and r1 == r3:
     print('O triângulo é EQUILÁTERO')
 elif r1 == r2 or r1 == r3 or r2 == r3:
     print('O triângulo é ISÓSCELES')
-else:
+elif r1 != r2 and r1 != r3 != r2 == 3:
     print('O triângulo é ESCALENO')
+else:
+    print('\033[31mOs segmentos acima não podem formar um triângulo\033[m')
+
+
 
 
 
