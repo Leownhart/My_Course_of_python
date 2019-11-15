50cceb2d22bcdd4f1330b45c879003987209dc36
Francisco <francisco.amartins.al@gmail.com>
2019-11-15 17:27:14 -0300

    Parcelamento de Compras

--- Exercicios/ex044.py
+++ Exercicios/ex044.py
@@ -6,21 +6,16 @@ print('''
   [ 4 ] 3x ou mais no cartão 
 ''')
 option = int(input('Qual é a opção? '))
-desconto1 = price * 10 / 100
-desconto2 = price * 5 / 100
-juros = price * 20 / 100
 if option == 1:
-    print('Sua compra de R${:.2f} vai custar R${:.2f} no final.'
-          .format(price, price - desconto1))
+    desconto = price * 10 / 100
+    print('Sua compra de R${:.2f} vai custar R${:.2f} no final.'.format(price, price - desconto))
 elif option == 2:
-    print('Sua compra de R${:.2f} vai custar R${:.2f} no final.'
-          .format(price, price - desconto2))
+    desconto = price * 5 / 100
+    print('Sua compra de R${:.2f} vai custar R${:.2f} no final.'.format(price, price - desconto))
 elif option == 3:
-    print('Sua compra de R${:.2f} vai custar R${:.2f} no final'
-          .format(price))
+    print('Sua compra de R${:.2f} vai custar R${:.2f} no final'.format(price))
 elif option == 4:
+    juros = price * 20 / 100
     parcela = int(input('Informe a quantidade de parcelas: '))
-    print('Sua compra de R${:.2f} vai custar R${:.2f} no fianl'
-          .format(price, price + juros))
-    print('Sua compra será parcelada em {}x de R${:.2f} com juros'
-          .format(parcela, (price + juros) / parcela))
+    print('Sua compra de R${:.2f} vai custar R${:.2f} no fianl'.format(price, price + juros))
+    print('Sua compra será parcelada em {}x de R${:.2f} com juros'.format(parcela, (price + juros) / parcela))
