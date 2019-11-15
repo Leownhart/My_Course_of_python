440c04fcd5de33345da151742b378f92ca4d0cc6
Francisco <francisco.amartins.al@gmail.com>
2019-11-15 15:31:43 -0300

    Imc

--- Exercicios/ex043.py
+++ Exercicios/ex043.py
@@ -3,14 +3,14 @@ Altura = float(input('Digite a altura (Mt): '))
 Peso = float(input('Digite o Peso (Kg): '))
 imc = Peso / pow(Altura, 2)
 if imc <= 18.5:
-    print('Imc = \033[34m{:.2f}\033[m , o mesmo está abaixo do peso: '
+    print('Imc = \033[34m{:.1f}\033[m , o mesmo está abaixo do peso: '
           .format(imc))
 elif imc < 25:
-    print('Imc = \033[34m{:.2f}\033[m , o mesmo está no peso ideal'
+    print('Imc = \033[34m{:.1f}\033[m , o mesmo está no peso ideal'
           .format(imc))
 elif imc < 30:
-    print('Imc = \033[34m{:.2f}\033[m , o mesmo está em Sobrepeso'
+    print('Imc = \033[34m{:.1f}\033[m , o mesmo está em Sobrepeso'
           .format(imc))
 else:
-    print('Imc = \033[34m{:.2f}\033[m , o mesmo está em obsidade mórbida'
+    print('Imc = \033[34m{:.1f}\033[m , o mesmo está em obsidade mórbida'
           .format(imc))
