a0b34833f1743f8fd9c750685148c843621b76c5
Francisco <francisco.amartins.al@gmail.com>
2019-11-28 15:15:40 -0300

    Estrutura While

--- Controle/While.py
+++ Controle/While.py
@@ -9,10 +9,22 @@ print('Fim')'''
 '''for c in range(1, 5):
     n = int(input('Digite um valor: '))
 print('Fim')'''
-n = 1
+'''n = 1
 while n != 0:  # Flag condição de parada.
     n = int(input('Digite um valor: '))
-print('Fim')
+print('Fim')'''
+
+n = 1
+par = 0
+impar = 0
+while n != 0:
+    n = int(input('Digite um valor: '))
+    if n % 2 == 0:
+         par += 1
+    else:
+        impar += 1
+print('Você digitou {} números pares e {} impares.'.format(par, impar))
+
 
 
 
