1bbb279584b9752bae8b4e6569fa09e40b6b00d7
Francisco <francisco.amartins.al@gmail.com>
2019-11-28 18:04:52 -0300

    multi program while

--- Exercicios/ex059.py
+++ Exercicios/ex059.py
@@ -1,2 +1,37 @@
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segundo número: '))
+opção = 0
+while opção != 5:
+    print('''
+      [1] somar
+      [2] multiplicar
+      [3] maior
+      [4] novos números
+      [5] sair do programa  
+    ''')
+    opção = int(input('Qual é a sua opção? '))
+    if opção == 1:
+        soma = n1 + n2
+        print('A soma entre {} e {} é {}'.format(n1,n2, soma))
+    elif opção == 2:
+        produto = n1 * n2
+        print('O resultado de {} x {} é {}'.format(n1, n2, produto))
+    elif opção == 3:
+        if n1 > n2:
+            maior = n1
+        else:
+            maior = n2
+        print('Entre {} e {} o maior valor é {}'.format(n1, n2, maior))
+    elif opção == 4:
+        print('Informe os números novamente: ')
+        n1 = int(input('Primeiro número: '))
+        n2 = int(input('Segundo número: '))
+    elif opção == 5:
+        print('Finalizando...')
+    else:
+        print('Opção inválida. Tente novamente')
+    print('=-='*10)
+print('Fim do programa! volte sempre!')
+
 
 
