9247564d0ae4d5a75899ef69d3fd6bf26197e2ea
Francisco <francisco.amartins.al@gmail.com>
2019-11-28 15:09:40 -0300

    Estrutura While

---
+++ Controle/While.py
@@ -0,0 +1,18 @@
+'''for c in range(1, 10):
+    print(c)
+print('Fim')'''
+'''c = 1
+while c < 10:
+    print(c)
+    c += 1
+print('Fim')'''
+'''for c in range(1, 5):
+    n = int(input('Digite um valor: '))
+print('Fim')'''
+n = 1
+while n != 0:  # Flag condição de parada.
+    n = int(input('Digite um valor: '))
+print('Fim')
+
+
+
