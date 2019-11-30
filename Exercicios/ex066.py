13d04e222b96ffa73c82bc0be31a2077cf542cd6
Francisco <francisco.amartins.al@gmail.com>
2019-11-30 16:43:43 -0300

    Lendo números utilizando Break

--- Exercicios/ex066.py
+++ Exercicios/ex066.py
@@ -1,10 +1,10 @@
 cont = soma = n = 0
 while n != 999:
-    n = int(input('Digite um número: '))
+    n = int(input('Digite um número (999 para parar): '))
     if n != 999:
         cont += 1
         soma += n
     if n == 999:
         break
-print(f'Quantidade de números digitados {cont}.')
-print(f'Soma total dos números digitados {soma}.')
+print(f'Quantidade de números digitados {cont}!.')
+print(f'Soma total dos números digitados {soma}!.')
