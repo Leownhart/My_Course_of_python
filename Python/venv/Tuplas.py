0e1cea4ddd23b0894b8b665ad70bc254dc2646e4
Francisco <francisco.amartins.al@gmail.com>
2020-01-10 18:17:15 -0300

    Tuplas

---
+++ Python/venv/Tuplas.py
@@ -0,0 +1,13 @@
+lanche = ('Hambúrguer', 'Pizza', 'Suco', 'Pudim', 'Batata Frita')
+print(lanche)
+print(len(lanche))
+# Tuplas são imutáveis
+# lanche[1] = 'Refrirante'
+print(lanche[1])
+# Não há possibilidade de atribuição de valores depois da declaração
+# da tupla
+#for comida in lanche:
+#    print(f'Eu vou comer {comida}')
+
+print('Comi pra caramba!')
+
