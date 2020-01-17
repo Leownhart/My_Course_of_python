39e8d206836cbc8a697695f424c430031e2464d2
Francisco <francisco.amartins.al@gmail.com>
2020-01-17 18:08:52 -0300

    Tupla AEIOU

---
+++ Exercicios/ex077.py
@@ -0,0 +1,10 @@
+vogais = ('APRENDER', 'PROGRAMAR', 'LINGUAGEM', 'PYTHON', 'CURSO', 'GRATIS', 'ESTUDAR',
+          'PRATICAR', 'TRABALHAR', 'MERCADO', 'PROGRAMADOR', 'FUTURO')
+
+for palavra in vogais:
+    print(f'\nNa palavra {palavra} temos ', end='')
+    for letra in palavra:
+        if letra.lower() in 'aeiou':
+            print(letra, end=' ')
+
+
