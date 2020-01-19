8f58ae6ff5115d8dddce7aa38b6a423d8a122ea6
Francisco <francisco.amartins.al@gmail.com>
2020-01-19 13:41:53 -0300

    Listas

---
+++ Python/Listas.py
@@ -0,0 +1,7 @@
+# lista =[], criando uma lista
+# .append(''), adiciona um elemento no final da lista
+# .insert(0,'elemento'), adiciona um elemento em lugar dsignado.
+# .pop(3), elemina um elemento.
+# .remove('elemento'), remove pelo elemento.
+# valores.sort(), Ordena
+# valores.sort(reverse=true), Ordena de modo reverso.
