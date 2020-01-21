9f0bb67102e9bc932af2d6e8d128b728a697ce35
Francisco <francisco.amartins.al@gmail.com>
2020-01-21 15:00:22 -0300

    recebendo dados do usuario

---
+++ Geek University/Seção 3/recebendo_dados_usuario.py
@@ -0,0 +1,53 @@
+"""
+Recebendo dados do usuário
+
+input() -> Todo dado recebido via input é do tipo String
+
+Em Python, string é tudo que estiver entre:
+- Aspas simples;
+- Aspas duplas;
+- Aspas simples triplas;
+- Aspas duplas triplas.
+
+Exemplos:
+- Aspas simples -> 'Angelina Jolie'
+- Aspas duplas -> "Angelina Jolie"
+- Aspas simples triplas -> '''Angelina Jolie'''
+"""
+# Entrada de dados
+print('Qual seu nome? ')
+nome = input()  # Input -> Entrada]
+
+# Exemplo de print 'Antigo' 2.x
+# print('Seja bem-vindo(a) %s' % nome)
+
+# Exemplo de print 'moderno' 3.x
+# print('Seja bem-vindo(a) {0}'.format(nome))
+
+# Exemplo de print 'mais atual' 3.7
+print(f'Seja bem-vindo(a) {nome}')
+
+print("Qual sua idade? ")
+idade = input()
+
+# Processamento
+
+# Saida
+# Exemplo de print 'antigo' 2.x
+# print('A %s tem %s anos' % (nome, idade))
+
+# Exemplo de print 'moderno' 3.x
+# print ('A {0} tem {1} anos'.format(nome, idade))
+
+# Exemplo de print 'mais atual' 3.7
+print(f'A {nome} tem {idade} anos')
+
+'''
+# int(idade) => cast
+
+Cast é a 'conversão' de um tipo de dado para outro.
+'''
+print(f'A {nome} nasceu em {2018 - int(idade)}')
+
+# Técnica mais pratica
+# nome = str(input('Digite um nome: '))
