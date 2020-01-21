346997836561dd5ef2c4a72ffa9ffc1094581de0
Francisco <francisco.amartins.al@gmail.com>
2020-01-21 19:28:50 -0300

    lista com pares e impares

---
+++ Exercicios/ex082.py
@@ -0,0 +1,19 @@
+a = []
+b = []
+c = []
+while True:
+    elemento = int(input(f'Informe um valor: '))
+    a.append(elemento)
+    opcao = str(input(f'Deseja continuar? [S/N] '))
+    if opcao.upper() == 'N':
+        break
+for n in range(0, len(a)):
+    if a[n] % 2 == 0:
+        b.append(a[n])
+    else:
+        c.append(a[n])
+print(f'A lista completa é {a}')
+print(f'A lista de pares é {b}')
+print(f'A lista de impares é {c}')
+
+
