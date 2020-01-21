484782dbc7658887d28fb18d30164d9ad76c9121
Francisco <francisco.amartins.al@gmail.com>
2020-01-20 22:16:20 -0300

    PEP8 Introdução

---
+++ Geek University/Seção 3/PEP8.py
@@ -0,0 +1,19 @@
+"""
+PEP8 - Python Enhancement Proposal
+São propostas de melhoria para a linguagem Python
+The Zen of Python
+Import This
+
+A ideia da PEP88 é que possamos escrever códigos Python de forma Pythónica.
+
+[1] - Utilize Camel Case para nomes de classes;
+[2] - Utilize nomes em minúsculo, separados por underline para funções ou variáveis;
+[3] - Utilize 4 espaços para identação!
+[4] - Separar funções e definições de classe com duas linha em branco;
+    - Métodos dentro de uma classe devem ser separados com uma única linha em branco;
+[5] - Imports devem ser sempre feitos em linhas separadas;
+[6] - Espaços em expressôes e instruçôes
+
+"""
+
+
