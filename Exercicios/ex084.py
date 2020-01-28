2fef8cee79a007bfe59ab2c8be1f9eb22608bb23
Francisco <francisco.amartins.al@gmail.com>
2020-01-28 13:48:10 -0300

    Melhor resolução

--- Exercicios/ex084.py
+++ Exercicios/ex084.py
@@ -1,4 +1,37 @@
-pes = []
+temp = []
+princ = []
+mai = men = 0
+while True:
+    temp.append(str(input('Nome: ')))
+    temp.append(float(input('Peso: ')))
+    if len(princ) == 0:
+        mai = men = temp[1]
+    else:
+        if temp[1] > mai:
+            mai = temp[1]
+        if temp[1] < men:
+            men = temp[1]
+    princ.append(temp[:])
+    temp.clear()
+    resp = str(input('Quer continuar? [S/N] '))
+    if resp in 'Nn':
+        break
+print('-='*30)
+print(f'Ao todo, voçê cadastrou {len(princ)} pessoas.')
+print(f'O maior peso foi de {mai}Kg. Peso de ', end='')
+for p in princ:
+    if p[1] == mai:
+        print(f'[{p[0]}]', end='')
+print()
+print(f'O menor peso foi de {men}Kg. Peso de ', end='')
+for p in princ:
+    if p[1] == men:
+        print(f'[{p[0]}] ', end='')
+print()
+
+'''(Solução Incorreta)'''
+
+'''pes = []  
 mail = []
 menl = []
 cont = mai = men = 0
@@ -7,7 +40,7 @@ while True:
     peso = float(input('Peso: '))
     pes.append(nome)
     pes.append(peso)
-    option = str(input('Quer continuar? ')).strip().upper()
+    option = str(input('Quer continuar [S/N]? ')).strip().upper()
     if option == 'N':
         break
 for i, v in enumerate(pes):
@@ -27,7 +60,8 @@ for i, v in enumerate(pes):
 print('-=' * 30)
 print(f'Ao todo, você cadastrou {cont} pessoas')
 print(f'O maior peso foi de {mai}Kg. Peso de {mail}')
-print(f'O menor peso foi de {men}Kg. Peso de {menl}')
+print(f'O menor peso foi de {men}Kg. Peso de {menl}')'''
+
 
 
 
