7519cdb0e7e915670dcb1db40882e1f8ff429556
Francisco <francisco.amartins.al@gmail.com>
2020-02-20 17:59:21 -0300

    Matriz Aprimorada

--- Exercicios/ex086.py
+++ Exercicios/ex086.py
@@ -1,6 +1,6 @@
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 for linha in range(0, 3):
-    for coluna in range(0,3):
+    for coluna in range(0, 3):
         matriz[linha][coluna] = int(input(f'Digite um valor para [{linha}, {coluna}]: '))
 print('-='*30)
 for linha in range(0, 3):
