bbf3e7a33a96c7a46e67fbd831c3142535d3f330
Francisco <francisco.amartins.al@gmail.com>
2020-02-26 16:56:29 -0300

    Perimetro Retângular utilizando Função

--- Exercicios/ex096.py
+++ Exercicios/ex096.py
@@ -1,4 +1,19 @@
 '''
 96 - Faça um programa que tenha uma função chamada área(),
 que receba as dimisões de um terreno retengular e comprimento) e mostre a área do terreno.
+
+F ==> 2(b + h)
+
 '''
+
+
+def área(larg, comp):
+    a = larg * comp
+    print(f'A área de um terreno ({larg} x {comp}) é de {a:.1f}².')
+
+
+print('Controle de Terrenos')
+print('-' * 20)
+larg = float(input('LARGURA (M): '))
+comp = float(input('COMPRIMENTO (M): '))
+área(larg, comp)
