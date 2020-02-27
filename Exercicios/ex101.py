51ff7fdccecd3e235d8c2342724787c8b8284b28
Francisco <francisco.amartins.al@gmail.com>
2020-02-27 17:00:15 -0300

    Idade com Função

--- Exercicios/ex101.py
+++ Exercicios/ex101.py
@@ -4,3 +4,21 @@ parâmetro o ano de nascimento de uma pessoa, retornan um valor literal indicand
 se uma pessoa tem voto NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleiçôes.
 
 '''
+
+
+def voto(nasc):
+    from datetime import datetime
+    anoatual = datetime.today().year
+    idade = anoatual - nasc
+    print('-'*30)
+    if idade <= 15:
+        return f'Com {idade} anos: VOTO NEGADO.'
+    elif 17 <= idade >= 65:
+        return f'Com {idade} anos: VOTO OPCIONAL.'
+    else:
+        return f'Com {idade} anos: VOTO OBRIGATÓRIO.'
+
+
+'''Programa Principal'''
+nas = int(input('EM que ano você nasceu? '))
+print(voto(nas))
