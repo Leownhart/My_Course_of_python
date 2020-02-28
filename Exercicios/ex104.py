df0fab12a298cc806f173baeb9ac03ecbd373691
Francisco <francisco.amartins.al@gmail.com>
2020-02-28 15:09:50 -0300

    Criando uma função para ler dados

--- Exercicios/ex104.py
+++ Exercicios/ex104.py
@@ -5,5 +5,24 @@ que fazendo a validação para aceitar apenas um valor numérico.
 
 
 Ex:
-n = leiaint('Digite um n')
+n = leiaint('Digite um número')
 '''
+
+
+def leiaInt(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print(' \033[0:31mERRO! Digite um número inteiro válido. \033[m')
+        if ok:
+            break
+    return valor
+
+
+n = leiaInt('Informe um número: ')
+print(f'Você acabou de digitar o número {n}')
