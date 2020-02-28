ef18c3b2d7337f41b02cae98d13f7bb61dac64fd
Francisco <francisco.amartins.al@gmail.com>
2020-02-28 18:10:37 -0300

    Criando uma função para ler dados

--- Exercicios/ex105.py
+++ Exercicios/ex105.py
@@ -1,6 +1,6 @@
 '''
 105 - Faça um programa que tenha uma função notas() que pode receber várias
-notass de alunos e vai retornar um dicionário com seguintes informçãoes:
+notas de alunos e vai retornar um dicionário com seguintes informações:
 
 - Quantidade de notas
 - A maior nota
@@ -10,3 +10,24 @@ notass de alunos e vai retornar um dicionário com seguintes informçãoes:
 
 Adicione também as docstrins da função.
 '''
+
+
+def notas(*resp, sit=False):
+    geral = dict()
+    geral['Total'] = len(resp)
+    geral['Maior Nota'] = max(resp)
+    geral['Menor Nota'] = min(resp)
+    geral['Média'] = sum(resp)/len(resp)
+    if sit:
+        if geral['Média'] >= 7:
+            geral['Situação'] = 'BOA'
+        elif geral['Média'] >= 5:
+            geral['Situação'] = 'Razóavel'
+        else:
+            geral['Situação'] = 'Ruim'
+    return geral
+
+
+# Programa Principal
+resp = notas(5.5, 2.5, 10, 6.5, sit=False)
+print(resp)
