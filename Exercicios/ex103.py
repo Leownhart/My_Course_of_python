3ec763bdee666ee74f3ddc14511daf04e644d773
Francisco <francisco.amartins.al@gmail.com>
2020-02-28 13:27:17 -0300

    Jogador Sorteio

--- Exercicios/ex103.py
+++ Exercicios/ex103.py
@@ -5,3 +5,21 @@ dois parâmetros opcionais: o nome de um jogador e quantos gols ele marcou.
 O programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum
 dado não tenha sido informado corretamente.
 '''
+
+
+def ficha(jog='<desconhecido>', gols=0):
+    print(f'O jogador {jog} fez {gols} gol(s) no campeonato.')
+
+
+# Programa principal
+nome = str(input('Nome do Jogador: '))
+gols = int(input('Número de Gols: '))
+
+if gols.isnumeric():
+    gols = int(g)
+else:
+    gols = 0
+if nome.strip() == '':
+    ficha(gols=gols)
+else:
+    ficha(nome, gols)
