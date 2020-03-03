b4c9cde8fc051715cee3e66050305890c10c9607
Leownhart <francisco.amartins.al@gmail.com>
2020-03-03 15:38:17 -0300

    urllib

--- Exercicios/ex114.py
+++ Exercicios/ex114.py
@@ -2,3 +2,13 @@
 114 - Crie um código em python que teste se o site Pudim
 está acessivel pelo computador usado.
 '''
+
+import urllib
+import urllib.request
+
+try: 
+    site = urllib.request.urlopen('http://www.pudim.com.br')
+except urllib.error.URLError:
+    print('O site Pudim  não está acessivel no momento.')
+else:
+    print('Tudo Ok')
