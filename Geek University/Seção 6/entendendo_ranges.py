dc54e4a63ebf9d33876ed3d98acb2f68c6a11078
Leownhart <francisco.amartins.al@gmail.com>
2020-06-18 18:35:39 -0300

    Conhecendo ranges

---
+++ Geek University/Seção 6/entendendo_ranges.py
@@ -0,0 +1,42 @@
+"""
+- Precisamos conhecer o loop for para utilizar o range.
+
+Ranges são utilizados para gerar sequências numéricas, não de forma aleatória,
+mas sim de maneira especifica.
+
+Formas gerais:
+
+# Forma 01
+
+range(valor_de_parada)
+
+OBS: valor_de_parada não inclusive (inicio padrão 0, e passo de 1 em 1)
+
+
+# Forma 1
+
+for num in range(11):
+    print(num)
+
+
+# Forma 2
+
+range(valor_de_inicio, valor_de_parada)
+
+
+OBS: valor_de_parada não inclusive (inicio especificado, e passo de 1 em 1)
+
+for num in range(1, 11)
+    print(num)
+
+
+# Forma 3
+
+range(valor_de_inicio, valor_de_parada, passo)
+
+OBS: Valor_de_parada não inclusive (inicio especificado pelo usuário, e passo  especificado pelo mesmo)
+
+for num in range(1, 10, 2)
+    print(num)
+    """
+
