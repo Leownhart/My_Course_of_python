3384e01f138cb4f2c5928b5eadbde7614cffe504
Leownhart <francisco.amartins.al@gmail.com>
2020-06-18 19:11:41 -0300

    Conhecendo while

---
+++ Geek University/Seção 6/loop_while.py
@@ -0,0 +1,33 @@
+'''
+Loop While
+
+For geral
+
+while expressão_booleana:
+    //Execução do Loop
+
+O bloco do while srá repetido enquanto a expressão bool for verdadeira.
+
+Expresão Bool é toda expressão onde o resultado é verdadeiro ou falso.
+
+
+Exemplo 1:
+
+num = 5
+num < 5
+
+
+numero = 1
+
+while numero < 10 <---- Condição de Parada
+    print(numero)
+    numero = numero + 1
+
+Exemplo 2:
+
+resposta = ''
+
+while resposta != 'sim':
+    resposa = input('Já acabou?')
+
+'''
